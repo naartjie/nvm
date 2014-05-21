65c491ece1bcd09186b938363d21d6b8988de6e1
Marcin Jekot <marcin@jekot.net>
2014-05-21 22:31:45 +0200

    quick hack to add auto-switching, copied straight from chruby project

---
+++ auto.sh
@@ -0,0 +1,32 @@
+unset NVM_AUTO_VERSION
+
+function nvm_auto() {
+
+  local dir="$PWD" version
+
+  until [[ -z "$dir" ]]; do
+    if { read -r version <"$dir/.nvmrc"; } 2>/dev/null || [[ -n "$version" ]]; then
+      if [[ "$version" == "$NVM_AUTO_VERSION" ]]; then return
+      else
+        NVM_AUTO_VERSION="$version"
+        nvm use "$version"
+        return $?
+      fi
+    fi
+
+    dir="${dir%/*}"
+  done
+
+  if [[ -n "$NVM_AUTO_VERSION" ]]; then
+    nvm use default
+    unset NVM_AUTO_VERSION
+  fi
+}
+
+# if [[ -n "$ZSH_VERSION" ]]; then
+  if [[ ! "$preexec_functions" == *nvm_auto* ]]; then
+    preexec_functions+=("nvm_auto")
+  fi
+# elif [[ -n "$BASH_VERSION" ]]; then
+#   trap '[[ "$BASH_COMMAND" != "$PROMPT_COMMAND" ]] && chruby_auto' DEBUG
+# fi
